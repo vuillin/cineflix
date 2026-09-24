4370e89a7e7d98dae3a973410b65f438dc3fc851
Thomas Vuillin <thomas.vuillin@proton.me>
2026-09-24 14:47:19 +0200

    refactor: remove unused hint text

--- views/letterboxd.php
+++ views/letterboxd.php
@@ -1,8 +1,5 @@
 <section id="view-letterboxd" class="view-section hidden">
     <div class="letterboxd-import">
-        <p class="letterboxd-import__hint">
-            Importe ton export Letterboxd (<code>watched.csv</code>) pour mettre à jour les dates de visionnage.
-        </p>
         <input type="file" id="letterboxd-csv-input" class="hidden" accept=".csv,text/csv">
         <button type="button" id="btn-letterboxd-import" class="btn-add-film">
             Importer watched.csv
